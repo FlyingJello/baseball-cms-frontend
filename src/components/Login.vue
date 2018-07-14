120681d8be16ac87c0a9ce45032d5170c89bc010
Jean-Philippe Lévesque <jeanphilippe.levesque1@gmail.com>
2018-07-14 16:43:15 -0400

    Adding login page, cmsHomePage and homePage.

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>hello welcome to my website</h1>
+        <p>here is the menu</p>
+        <ul>
+            <li><router-link to="/cms/home">CMS HOME</router-link></li>
+        </ul>
+        <p>and here will be the content :</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
