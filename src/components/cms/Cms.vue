1a9ebf556a87c1c391b598fec837fc7db2e61eba
Jean-Philippe Lévesque <jeanphilippe.levesque1@gmail.com>
2019-01-29 20:35:44 -0500

    Amélioration du menu et du dashboard

--- src/components/cms/Cms.vue
+++ src/components/cms/Cms.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-md bg-red navbar-dark fixed-top d-xs-block">
-      <a class="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-md bg-red navbar-dark fixed-top d-xs-block px-0 py-0">
+      <a class="navbar-brand pl-2 pl-sm-3 mx-0 py-2" href="#">
         <span>Baseball mineur de La Pocatière</span>
       </a>
-      <ul class="nav navbar-nav ml-auto">
-        <li class="px-2 py-1"><i style="color:white;" class="fas fa-sign-out-alt fa-lg"></i></li>
+      <ul id="top-container" class="nav navbar-nav ml-auto">
+        <li class="px-4 py-3"><i style="color:white;" class="fas fa-cog fa-lg"></i></li>
       </ul>
     </nav>
     <nav class="fixed-bottom navbar-expand dark-grey-background d-xs-block d-sm-none">
       <div class="row" style="margin-left: 0px; margin-right: 0px;">
         <a class="col text-center py-3 list-group-item-xs active">
-          <span class="fas fa-tachometer-alt fa-lg"></span>
+          <span class="fas fa-th-large fa-lg"></span>
         </a>
         <a class="col text-center py-3 list-group-item-xs">
           <span class="fab fa-whmcs fa-lg"></span>
@@ -22,19 +22,22 @@
         <a class="col text-center py-3 list-group-item-xs">
           <span class="fas fa-star fa-lg"></span>
         </a>
+        <a class="col text-center py-3 list-group-item-xs">
+          <span class="fas fa-star fa-lg"></span>
+        </a>
       </div>
     </nav>
 
     <div class="row" id="body-row">
-      <nav id="sidebar-container" class="sidebar-expanded d-none d-sm-block col-lg-2">
+      <nav id="sidebar-container" class="sidebar-expanded d-none d-sm-block col-xl-2">
         <ul class="list-group sticky-top">
-          <li class="d-none d-lg-block">
-            <div class="text-center py-3 d-none d-lg-block">
+          <li class="d-none d-xl-block">
+            <div class="text-center py-4 d-none d-xl-block">
               <div class="py-1">
-                <img src="..\..\assets\img_avatar.png" alt="Avatar" style="width:75px; border-radius: 50%;">
+                <img src="..\..\assets\img_avatar.png" alt="Avatar" style="width:85px; border-radius: 50%;">
               </div>
               <div class="pt-1">
-                <span style="color:white;">Jean-Philippe Lévesque</span>
+                <span style="color:white; font-size: 16px;">Jean-Philippe Lévesque</span>
               </div>
               <div>
                 <span style="color:#aaa;">Administrateur</span>
@@ -49,8 +52,8 @@
           <router-link to="home">
             <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
               <div class="d-flex w-100 justify-content-start align-items-center">
-                <span class="fas fa-tachometer-alt fa-fw mr-3"></span>
-                <span class="d-none d-lg-block">Tableau de bord</span>
+                <span class="fas fa-th-large fa-fw mr-3"></span>
+                <span class="d-none d-xl-block">Tableau de bord</span>
               </div>
             </a>
           </router-link>
@@ -63,7 +66,7 @@
             <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
               <div class="d-flex w-100 justify-content-start align-items-center">
                 <span class="far fa-calendar-alt fa-fw mr-3"></span>
-                <span class="d-none d-lg-block">
+                <span class="d-none d-xl-block">
                   Gestion des événements
                 </span>
               </div>
@@ -73,13 +76,13 @@
           <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="far fa-newspaper fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Gestion des nouvelles</span>
+              <span class="d-none d-xl-block">Gestion des nouvelles</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-users fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Gestion des joueurs</span>
+              <span class="d-none d-xl-block">Gestion des joueurs</span>
             </div>
           </a>
 
@@ -90,25 +93,25 @@
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-file-import fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Importations et exportations</span>
+              <span class="d-none d-xl-block">Importations et exportations</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="far fa-chart-bar fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Statistiques</span>
+              <span class="d-none d-xl-block">Statistiques</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-handshake fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Commandites</span>
+              <span class="d-none d-xl-block">Commandites</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-address-card fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Contact</span>
+              <span class="d-none d-xl-block">Contact</span>
             </div>
           </a>
 
@@ -119,25 +122,25 @@
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-tasks fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Gestion des tâches</span>
+              <span class="d-none d-xl-block">Gestion des tâches</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-users-cog fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Gestion du personnel</span>
+              <span class="d-none d-xl-block">Gestion du personnel</span>
             </div>
           </a>
           <a href="#" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-start align-items-center">
               <span class="fas fa-history fa-fw mr-3"></span>
-              <span class="d-none d-lg-block">Historique des transactions</span>
+              <span class="d-none d-xl-block">Historique des transactions</span>
             </div>
           </a>
         </ul>
       </nav>
-      <div id="content" class="col-lg-10 pt-3">
-        <router-view/>
+      <div id="content" class="col-xl-10">
+          <router-view/>       
       </div>
     </div>
   </div>
--- src/components/cms/Dashboard.vue
+++ src/components/cms/Dashboard.vue
@@ -1,34 +1,45 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-12 col-lg-6 mb-4">
+    <div class="row mt-3">
+      <div class="col-12 col-lg-6 mb-3 px-0 mb-sm-3 px-sm-3">
         <div class="col-12 white-section">
-          <div class="py-3 mb-0 mx-2">
-            <div class="row px-0 mx-0">
-              <div class="col-12 px-0">
-                <h4>Événements de la semaine</h4>
+          <div class="py-3">
+            <div class="row px-1 mx-0">
+              <div class="col-12">
+                <div class="row">
+                  <div class="col-11 px-0">
+                    <h2>Dernières activités</h2>
+                  </div>
+                  <div class="col-1 px-0 text-right">
+                    <i class="fas fa-ellipsis-v"></i>
+                  </div>
+                </div>
               </div>
-              <div class="col-12 px-0 pt-2">
-                <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
+              <div class="row">
+                <div class="col-12 pt-2">
+                  <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-12 col-lg-6 mb-4">
+      <div class="col-12 col-lg-6 mb-3 px-0 mb-sm-3 px-sm-3">
         <div class="col-12 white-section">
-          <div class="py-3 mb-0 mx-2">
-            <div class="row px-0 mx-0">
-              <div class="col-11 px-0">
-                <h5>Dernières activités</h5>
+          <div class="py-3">
+            <div class="row px-1 mx-0">
+              <div class="col-12">
+                <div class="row">
+                  <div class="col-11 px-0">
+                    <h2>Dernières activités</h2>
+                  </div>
+                  <div class="col-1 px-0 text-right">
+                    <i class="fas fa-ellipsis-v"></i>
+                  </div>
+                </div>
               </div>
-              <div class="col-1 px-0 text-right">
-                <i class="fas fa-expand"></i>
-              </div>
-            </div>
-            <div class="row px-0 mx-0">
-              <div class="col-12 px-0 pt-2">
-                <div class="row px-0 mx-0" style="overflow: auto;">
+              <div class="row">
+                <div class="col-12 pt-2">
                   <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
                 </div>
               </div>
@@ -38,12 +49,12 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-12 mb-4 col-lg-4">
+      <div class="col-12 col-lg-4 mb-3 px-0 mb-sm-3 px-sm-3 pr-lg-1 pl-lg-3">
         <div class="col-12 white-section">
           <div class="py-3 mb-0 mx-2">
             <div class="row px-0 mx-0">
               <div class="col-12 px-0">
-                <h4>Titre de la section</h4>
+                <h2>Titre de la section</h2>
               </div>
               <div class="col-12 px-0 pt-2">
                 <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
@@ -52,12 +63,12 @@
           </div>
         </div>
       </div>
-      <div class="col-12 mb-4 col-lg-4">
+      <div class="col-12 col-lg-4 mb-3 px-0 mb-sm-3 px-sm-3 pr-lg-1 pl-lg-3">
         <div class="col-12 white-section">
           <div class="py-3 mb-0 mx-2">
             <div class="row px-0 mx-0">
               <div class="col-12 px-0">
-                <h4>Titre de la section</h4>
+                <h2>Titre de la section</h2>
               </div>
               <div class="col-12 px-0 pt-2">
                 <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
@@ -66,12 +77,12 @@
           </div>
         </div>
       </div>
-      <div class="col-12 mb-4 col-lg-4">
+      <div class="col-12 col-lg-4 mb-3 px-0 mb-sm-3 px-sm-3 px-lg-3">
         <div class="col-12 white-section">
           <div class="py-3 mb-0 mx-2">
             <div class="row px-0 mx-0">
               <div class="col-11 px-0">
-                <h5>Dernières activités</h5>
+                <h2>Dernières activités</h2>
               </div>
               <div class="col-1 px-0 text-right">
                 <i class="fas fa-expand"></i>
@@ -79,7 +90,7 @@
             </div>
             <div class="row px-0 mx-0">
               <div class="col-12 px-0 pt-2">
-                <div class="row px-0 mx-0" style="overflow: auto;">
+                <div class="row px-0 mx-0">
                   <p class="mb-0">On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
                 </div>
               </div>
